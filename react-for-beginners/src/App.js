771fe1f4bc6abe091aecf39d96bf7a35c91c9ecb
i-mnam <66421797+Heesu974@users.noreply.github.com>
2022-12-25 10:45:28 +0900

    state가 변화하면 모든 코드가 재 실행되는데, 특정코드는 원하는 때에 실행 - useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,37 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log('I run when "keyword" changes');
+    }
+  }, [keyword]);
+  //keyword가 change할 때만 useEffect가 실행
+  useEffect(() => {
+    if (counter !== 0) {
+      console.log('I run when "counter" changes');
+    }
+  }, [counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>할 수 있다.</h1>
-      <Button text={12} />
+      <h1>할 수 있다.</h1>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="search here..."
+      />
+      <h3>{counter}</h3>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
