a8b1fb41d0c68dca52d9dda0700fa831874a3dee
DESKTOP-V4BT0E4\cnt_24 <heesu.kang95@gmail.com>
2022-12-26 10:52:42 +0900

    then을 사용한 movie 목록 response

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,29 +1,31 @@
-import { useState, useEffect } from "react";
+import React, {useState, useEffect} from 'react';
 
-function App() {
+// "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+
+function App () {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
+    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+    .then(response => response.json()).then(json => {
+      setMovies(json.data.movies);
+      setLoading(false);
+    })
+  }, [])
+  console.log(movies)
   return (
     <div>
-      <h1>The coins ! {loading ? "" : `${coins.length}`}</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => (
-          <li>
-            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price}
-          </li>
-        ))}
-      </ul>
+
+      {loading ? <h1>Loading ... </h1> : null}
     </div>
-  );
+  )
 }
 
 export default App;
+
+//aim . 영화 목록을 보여주고, 정보를 넣어주고, 링크를 걸어준다. 해당 영화에 대해 더 많은 것을 찾아볼 수 있도록 앱에서 다른 곳으로 연결까지
+// aim. 앱 안에서 페이지 전환하는 방법까지
+//1.. 로딩인 것을 먼저 보여주고.
+//2. component가 시작할 때만 특정 코드를 실행하고 싶다.
+//then보다 더 보편적으로 사용하는게 있다. 이거 대신 async-await을 사용합니다.
+//async-await을 사용하기 위해 getMovies라는 함수를 만듭니다.
