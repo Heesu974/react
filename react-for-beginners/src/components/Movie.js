f87b8f6474bc4a47fb05e0b27c341ac5efe91b72
DESKTOP-V4BT0E4\cnt_24 <heesu.kang95@gmail.com>
2022-12-26 17:52:18 +0900

    파라미터 중

--- react-for-beginners/src/components/Movie.js
+++ react-for-beginners/src/components/Movie.js
@@ -31,14 +31,13 @@ Movie.prototype = {
 
 export default Movie;
 
-// /movie/:id로 연결하기 위해서는 Movie component에서 id를 연결해야 하는데
-//현재의 movie component에서는 props 로 id를 받지 않고 있습니다.
-//이 말은 즉슨, Movie 컴포넌트의 부모 요소 즉, Home component를 살펴봐야 한다는 건데, 
-//Home이 바로 Movie 컴포넌트를 렌더링하는 녀석이고,  Home component가 id를 prop으로 넘겨주고 있지 않습니다.
-//그래서 Home에서 prop으로 id를 먼저 넘겨줘야 합니다.
+//그렇다면 {`/movie/${id}`} 여기에 어떤 id가 오는지 알 수 있다면? 
+//이 url에 요청을 보낼 수 있기 때문에 매우 유용한 발전이 될 수 있다.
 
-//component를 렌더링한다는 것은, 실질적으로 함수를 불러오는 것이다.
-//props는 object일 뿐이고, 우리는 그걸 열어서 item을 꺼내 쓰는 것입니다.
+
+//   /movie/:id 는 React Router한테 여기 오는 id값이 뭔지 알고 싶다고 말하는 것이다.
+// url에 있는 값을 반환해주는 함수를 이용 - useParams
+
 
 
 
--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {useParams} from 'react-router-dom';
 
 function Detail() {
+    const {id} = useParams();
+    useEffect(() => {
+        
+    }, [])
+    console.log(id)
+
     return <h1>Detail</h1>
 }
 export default Detail;
@@ -15,3 +22,8 @@ export default Detail;
 
 
 //aim - react-router-dom 사용법
+
+// 현재 url이 
+//https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year 이건데,
+//여기서 어떠함 것에도 간섭받지 않는다고? - useEffect의 []를 통해서, 
+//8분에서
